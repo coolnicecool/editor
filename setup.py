a124cecf3be829293bb7666d1ea1dfb59dbba717
coolnicecool <raleigh@1337maps.com>
2022-05-24 20:37:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="pykernel",
-    version="0.1.4",
+    version="0.1.5",
     description="Python & text Windows CMD Editior written with only native libaries",
     long_description="2 Python Files With No Dependence that allow you to edit any file through keyboard or mouse. Allows you to run python files and paste/copy/cut/highlight",
     py_modules=["pykernel","TIME"],
